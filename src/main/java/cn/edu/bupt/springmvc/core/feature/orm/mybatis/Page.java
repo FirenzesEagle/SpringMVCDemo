aa0408d1559402879c0b6d648c60df0dd29f49f7
FirenzesEagle <liumingbo2008@gmail.com>
2016-04-11 10:58:53 +0800

    src file push

---
+++ src/main/java/cn/edu/bupt/springmvc/core/feature/orm/mybatis/Page.java
@@ -0,0 +1,16 @@
+package cn.edu.bupt.springmvc.core.feature.orm.mybatis;
+
+import org.apache.ibatis.session.RowBounds;
+
+/**
+ * Mybatis分页参数及查询结果封装. 注意所有序号从1开始.
+ *
+ * @param <T>   Page中记录的类型.
+ *
+ * Created by FirenzesEagle on 2016/4/11 0011.
+ */
+public class Page<T> extends RowBounds {
+
+
+
+}
