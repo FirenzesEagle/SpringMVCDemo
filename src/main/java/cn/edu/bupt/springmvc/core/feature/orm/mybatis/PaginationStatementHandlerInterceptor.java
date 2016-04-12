6e726bc5cee25873180e09fa7fde260a3e2063d5
FirenzesEagle <liumingbo2008@gmail.com>
2016-04-12 19:14:57 +0800

    del test

--- src/main/java/cn/edu/bupt/springmvc/core/feature/orm/mybatis/PaginationStatementHandlerInterceptor.java
+++ src/main/java/cn/edu/bupt/springmvc/core/feature/orm/mybatis/PaginationStatementHandlerInterceptor.java
@@ -1,14 +1,19 @@
 package cn.edu.bupt.springmvc.core.feature.orm.mybatis;
 
+import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.apache.ibatis.reflection.factory.ObjectFactory;
 import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
 import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,8 +23,8 @@ import java.util.Properties;
 /**
  * Created by FirenzesEagle on 2016/4/12 0012.
  */
-@Intercepts({@Signature(type = StatementHandler.class,method = "prepare",args = {Connection.class})})
-public class PaginationStatementHandlerInterceptor implements Interceptor{
+@Intercepts({@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class})})
+public class PaginationStatementHandlerInterceptor implements Interceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(PaginationStatementHandlerInterceptor.class);
 
@@ -28,6 +33,18 @@ public class PaginationStatementHandlerInterceptor implements Interceptor{
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        ParameterHandler parameterHandler = statementHandler.getParameterHandler();
+        BoundSql boundSql = statementHandler.getBoundSql();
+
+        MetaObject metaStatementHandler = MetaObject.forObject(statementHandler,DEFAULT_OBJECT_FACTORY,DEFAULT_OBJECT_WRAPPER_FACTORY);
+        RowBounds rowBounds = (RowBounds) metaStatementHandler.getValue("delegate.rowBounds");
+        //如果没有分页参数
+        if (rowBounds == null || rowBounds == RowBounds.DEFAULT){
+            return invocation.proceed();
+        }
+
+        Configuration configuration = (Configuration) metaStatementHandler.getValue("delegate.configuration");
         return null;
     }
 
