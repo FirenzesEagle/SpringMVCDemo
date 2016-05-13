8c894210b7cb5a6f35dd848b3b480460b0030646
FirenzesEagle <liumingbo2008@gmail.com>
2016-05-13 15:40:39 +0800

    add selcet by page

--- src/main/java/cn/edu/bupt/springmvc/web/service/UserService.java
+++ src/main/java/cn/edu/bupt/springmvc/web/service/UserService.java
@@ -3,6 +3,8 @@ package cn.edu.bupt.springmvc.web.service;
 import cn.edu.bupt.springmvc.core.generic.GenericService;
 import cn.edu.bupt.springmvc.web.model.User;
 
+import java.util.List;
+
 /**
  * Created by FirenzesEagle on 2016/4/18 0018.
  */
@@ -23,5 +25,7 @@ public interface UserService extends GenericService<User, Long> {
      * @return
      */
     User selectByUsername(String username);
+
+    List<User> selectByPage(int pageNo,int pageSize);
 
 }
